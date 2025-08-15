099607de1bffb8a0b485778ef31a47ec08c97752
Clint Shuman <cshuman@users.noreply.github.com>
2025-08-15 02:10:01 -0500

    Adding hotreload example (#3)
    * adding hotreload example (was originally in github.com/starfederation/datastar). updating go.mod deps
    
    * use the main branch for cdn
    
    ---------
    
    Co-authored-by: Clint Shuman <crshuman@gmail.com>

---
+++ cmd/examples/hotreload/generate-hotreload-html/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	cdn = "https://cdn.jsdelivr.net/gh/starfederation/datastar@main/bundles/datastar.js"
+)
+
+// generates an hotreload.html file to be used to play with the hotreload example
+func main() {
+	os.WriteFile("hotreload.html", []byte(fmt.Sprintf(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+	<script type="module" defer src="%s"></script>
+</head>
+
+<!-- next line mounts the refresh script -->
+<body data-on-load="@get('/hotreload', {retryInterval: 100})">
+	<main>
+		<p>
+			This page will automatically reload on any change to the hotreload.html file. Update this paragraph, save changes, and
+			switch back to the open browser tab to observe
+			the update.
+		</p>
+	</main>
+</body>
+
+</html>`, cdn)), 0644)
+}
